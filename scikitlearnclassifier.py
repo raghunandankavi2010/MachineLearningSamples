10ff0e7f96880a4a42f5c37bb4f627b41980ddbb
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2020-03-26 14:59:27 +0530

    Writing our own classifier using eucledian distance.

--- scikitlearnclassifier.py
+++ scikitlearnclassifier.py
@@ -1,24 +1,58 @@
+from scipy.spatial import distance
+
+
+def euc(a, b):
+    return distance.euclidean(a, b)
+
+
+class ScrappyKNN():
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X_test):
+        predictions = []
+        for row in X_test:
+            label = self.closest(row)
+            predictions.append(label)
+
+        return predictions
+
+    def closest(self, row):
+        # Distance from test point to first training point
+        best_dist = euc(row, self.X_train[0])  # Shortest distance found so far
+        best_index = 0  # index of closest training point
+        for i in range(1, len(self.X_train)):  # Iterate over all other training points
+            dist = euc(row, self.X_train[i])
+            if dist < best_dist:  # Found closer, update
+                best_dist = dist
+                best_index = i
+        return self.y_train[best_index]  # closest example
+
+
 from sklearn import datasets
 
 iris = datasets.load_iris()
 
-# X is input and Y is output. X is data and Y is label here
-x = iris.data
+X = iris.data
 y = iris.target
 
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x, y, test_size =.5)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 
 # from sklearn import tree
 # my_classifier = tree.DecisionTreeClassifier()
 
 # alternate kneightborsclassifier
-from sklearn.neighbors import KNeighborsClassifier
-my_classifier = KNeighborsClassifier
+# from sklearn.neighbors import KNeighborsClassifier
 
-my_classifier.fit(x_train,y_train)
+my_classifier = ScrappyKNN()
 
-predictions = my_classifier.predict(x_test)
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
 
 from sklearn.metrics import accuracy_score
-print (accuracy_score(y_test,predictions))
+
+print(accuracy_score(y_test, predictions))
