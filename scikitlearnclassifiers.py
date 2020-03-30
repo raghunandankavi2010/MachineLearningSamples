10fc66c64af0c4698b66630052dd661767c479de
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2020-03-30 13:36:19 +0530

    Minor Change.

--- scikitlearnclassifiers.py
+++ scikitlearnclassifiers.py
@@ -7,7 +7,7 @@ x = iris.data
 y = iris.target
 
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x, y, test_size =.5)
+x_train,x_test,y_train,y_test = train_test_split(x, y, .5)
 
 from sklearn import tree
 my_classifier = tree.DecisionTreeClassifier()
