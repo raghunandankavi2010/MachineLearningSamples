db1c135972ff2d562029d1c5bbff1033538da897
raghu <raghunandankavi2010@gmail.com>
2023-04-02 12:30:07 +0530

    Adding comment

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,7 @@
 from sklearn import tree
 features = [[140,1],[130,1],[150,0],[170,0]]
 labels = ["0","0","1","1"]
+##Dececion tree like if else
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features,labels)
 print(clf.predict([[150,0]]))
