fcfc694e3bb31b67416510a0db1b74aace8a0035
raghu <raghunandankavi2010@gmail.com>
2023-04-02 12:46:37 +0530

    Adding comment with formatting

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,8 @@
 from sklearn import tree
-features = [[140,1],[130,1],[150,0],[170,0]]
-labels = ["0","0","1","1"]
-##Dececion tree like if else
+
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = ["0", "0", "1", "1"]
+# Decision tree like if else
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features,labels)
-print(clf.predict([[150,0]]))
+clf = clf.fit(features, labels)
+print(clf.predict([[150, 0]]))
